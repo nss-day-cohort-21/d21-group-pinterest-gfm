340ee767774bbb95e21a7feaead08cbd80d3acfa
Fang <fang-wen.shen@vanderbilt.edu>
2017-09-05 15:25:08 -0500

    made firebase factory

---
+++ app/factories/FBFactory.js
@@ -0,0 +1,136 @@
+"use strict";
+angular.module('myApp').factory("FBFactory", function($q, $http, firebaseInfo){
+    const getAllPins = function(userId){
+        let notes = [];
+        return $q( (resolve, reject) => {
+            $http.get(`${firebaseInfo.databaseURL}/pins.json?orderBy="uid"&equalTo="${userId}"`)
+            .then((itemObject) => {
+                notes = itemObject.data;
+                resolve(notes);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    };
+    const getSinglePin = function(boardId){
+        let notes = [];
+        return $q( (resolve, reject) => {
+            $http.get(`${firebaseInfo.databaseURL}/pins.json?orderBy="uid"&equalTo="${boardId}"`)
+            .then((itemObject) => {
+                notes = itemObject.data;
+                resolve(notes);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    };
+    const postPin = (note) =>{
+        if (firebase.auth().currentUser) {
+            let newnote = JSON.stringify(note);
+            return $q( (resolve, reject) => {
+                $http.post(`${firebaseInfo.databaseURL}/pins.json`,newnote)
+                .then((response) => {
+       
+                    resolve(response);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            });
+        }
+    };
+    const patchPin = function(id, obj) {
+        return $q((resolve, reject) => {
+            let newObj = JSON.stringify(obj);
+            $http.patch(`${firebaseInfo.databaseURL}/pins/${id}.json`, newObj)
+            .then((data) => {
+                resolve(data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        });
+    };    
+    const deletePin = function(uglyId){
+        return $q( (resolve, reject) => {
+            $http.delete(`${firebaseInfo.databaseURL}/pins/${uglyId}.json`)
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        });
+    };
+    const getAllBoards = (uid) =>{
+        if (firebase.auth().currentUser) {
+            let newnote = JSON.stringify(note);
+            return $q( (resolve, reject) => {
+                $http.post(`${firebaseInfo.databaseURL}/boards.json?orderBy="uid"&equalTo="${boardId}"`)
+                .then((response) => {
+       
+                    resolve(response);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            });
+        }
+    };
+    const getSingleBoard = (boardId) =>{
+        if (firebase.auth().currentUser) {
+            let newnote = JSON.stringify(note);
+            return $q( (resolve, reject) => {
+                $http.post(`${firebaseInfo.databaseURL}/boards/${boardId}.json`)
+                .then((response) => {
+       
+                    resolve(response);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            });
+        }
+    };
+    const postBoard = (boardId) =>{
+        if (firebase.auth().currentUser) {
+            let newnote = JSON.stringify(note);
+            return $q( (resolve, reject) => {
+                $http.post(`${firebaseInfo.databaseURL}/boards/.json`,newnote)
+                .then((response) => {
+       
+                    resolve(response);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            });
+        }
+    };
+    const patchBoard = function(id, obj) {
+        return $q((resolve, reject) => {
+            let newObj = JSON.stringify(obj);
+            $http.patch(`${firebaseInfo.databaseURL}/boards/${id}.json`, newObj)
+            .then((data) => {
+                resolve(data);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        });
+    };
+    const deleteBoard = function(uglyId){
+        return $q( (resolve, reject) => {
+            $http.delete(`${firebaseInfo.databaseURL}/boards/${uglyId}.json`)
+            .then((response) => {
+                resolve(response);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        });
+    };
+    return {getAllPins,getSinglePin,postPin,patchPin,deletePin,getAllBoards,getSingleBoard,postBoard,patchBoard,deleteBoard};
+});
